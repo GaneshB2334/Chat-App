b83f66f63c11fc1f38b6e230a1baeab37a6c6e06
ganeshb2334 <bastapureganesh21@gmail.com>
2025-03-29 16:19:14 +0530

    Updated skeleton

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthContextProvider>
         <App />
     </AuthContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 )
