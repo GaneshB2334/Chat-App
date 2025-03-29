99cbdc093ed6b5ab40840b4386c0c41d39cd48ec
ganeshb2334 <bastapureganesh21@gmail.com>
2025-03-29 15:16:15 +0530

    First commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,15 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "https://chat-app-ku8j.onrender.com",
-				changeOrigin: true,
-				secure: true,
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "https://chat-app-ku8j.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+    host: true,
+  },
 });
